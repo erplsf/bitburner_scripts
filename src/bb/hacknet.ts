509c65da931ee78cedf5bfaa47fbe9d66c649c4c
Andriy Mykhaylyk <andriy.mykhaylyk@klar.mx>
2021-12-27 13:58:28 +0100

    todo

--- src/bb/hacknet.ts
+++ src/bb/hacknet.ts
@@ -2,12 +2,25 @@ import { NS } from "../../bitburner/src/ScriptEditor/NetscriptDefinitions";
 
 /** @param {NS} ns **/
 export async function main(ns: NS): Promise<void> {
+    // TODO: fix/swap to correct calculating and stuff
     const nodeCount = ns.hacknet.numNodes()
+    let uCost = 0
+    const upgrades: [number, string, number][] = []
     for(let i = 0; i < nodeCount; i++) {
         let pMoney = ns.getPlayer().money
 
         let lCost = ns.hacknet.getLevelUpgradeCost(i, 1)
-        while (lCost != Infinity && lCost / pMoney <= 0.05) {
+        uCost += lCost
+        if(lCost != Infinity && uCost / pMoney <= 0.1) {
+            const found = upgrades.findIndex(u => u[0] == i)
+            if(found != -1) {
+                upgrades[found][2] += 1
+            } else {
+                upgrades.push([i, 'c', 1])
+            }
+        }
+
+        while (lCost != Infinity && uCost / pMoney <= 0.1) {
             ns.hacknet.upgradeLevel(i, 1)
             lCost = ns.hacknet.getLevelUpgradeCost(i, 1)
             pMoney = ns.getPlayer().money
@@ -26,5 +39,8 @@ export async function main(ns: NS): Promise<void> {
             cCost = ns.hacknet.getCoreUpgradeCost(i, 1)
             pMoney = ns.getPlayer().money
         }
+    }
+    for(const up of upgrades) {
+        const [i, t, l] = up
     }
 }
